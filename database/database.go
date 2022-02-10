8115b63bc8fb17809eab3532099983ae024ecf8a
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-02-10 15:02:30 -0300

    add a env vars getter

--- database/database.go
+++ database/database.go
@@ -3,12 +3,13 @@ package database
 import (
 	"fmt"
 
+	"prueba/utils"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-var url = "uaeglbv6jfjd8ukb:gCyC6IQnGpjJ8JQnTIJT@tcp(bys4hq6m8jrd9fovr6hz-mysql.services.clever-cloud.com:3306)/bys4hq6m8jrd9fovr6hz"
-
+var url = utils.GetEnv("DB_URL")
 var Connection = func() *gorm.DB {
 	db, err := gorm.Open(mysql.Open(url), &gorm.Config{})
 	if err != nil {
